ea4867e688cb959f4ad5bbd99ff470fc657f9470
mathilde-schveitzer <mathilde.schvtzr@gmail.com>
2021-05-07 10:24:45 +0200

    nvx test

--- kratos_trainer.py
+++ kratos_trainer.py
@@ -9,7 +9,7 @@ from model import *
 import os
 
 def main(name,identifiant,device='cpu'):
-    nlimit=2
+    nlimit=5
 
     filename='nbeats_f100/train/SAT2_10_minutes_future100_{}.csv'.format(identifiant)
     filename_='nbeats_f100/test/SAT2_10_minutes_future100_4.csv'
@@ -17,8 +17,8 @@ def main(name,identifiant,device='cpu'):
     train_set=gs.register_signal(filename).transpose() #[time_step]x[dim] > [dim]x[time_step]
     test_set=gs.register_signal(filename_).transpose()
 
-    train_set=normalize_data(train_set[nlimit-1:nlimit,:])
-    test_set=normalize_data(test_set[nlimit-1:nlimit,:])
+    train_set=normalize_data(train_set[nlimit:nlimit+1,:])
+    test_set=normalize_data(test_set[nlimit:nlimit+1,:])
 
     path='./data/{}'.format(name)
 
@@ -28,7 +28,7 @@ def main(name,identifiant,device='cpu'):
 
     backast_length=100
     forecast_length=100
-    nb=5500
+    nb=1000
     
     xtrain,ytrain,xtest,ytest=get_data2(backast_length, forecast_length, nb, train_set, test_set)
     print('we got the data : xtrain.shape :', xtrain.shape)
@@ -39,12 +39,12 @@ def main(name,identifiant,device='cpu'):
     nlayers=2
     nMLP=128
     nhead=4
-    dropout=0.5
-    epochs=500
+    dropout=0.2
+    epochs=200
     bsz=256
     eval_bsz=256
     
-    model=TransformerModel(ninp, nhead, nhid, nlayers, nMLP, backast_length, forecast_length, dropout=dropout, device=device)
+    model=TransformerModel(ninp, nhead, nhid, nlayers, nMLP, backast_length, forecast_length,pos_encod=False, dropout=dropout, device=device)
     
     print("Model structure: ", model, "\n\n")
     for layer_name, param in model.named_parameters():
