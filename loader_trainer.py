255525d5d10159b8355f534b853d7f562fca165b
mathilde-schveitzer <mathilde.schvtzr@gmail.com>
2021-06-09 11:45:04 +0200

    nv test (avec time)

--- loader_trainer.py
+++ loader_trainer.py
@@ -11,18 +11,18 @@ def main(name,storage,ninp,device='cpu'):
     
 
     name_='nbeats_tr_{}'.format(storage)
-  #  name1='nbeats_fc_{}'.format(storage)
+    name1='nbeats_fc_{}'.format(storage)
   #  name2='transformer_{}'.format(storage)
 
     storage_path='./data/{}'.format(name_)
- #   storage_path_='./data/{}'.format(name1)
+    storage_path_='./data/{}'.format(name1)
  #   storage_path__='./data/{}'.format(name2)
 
     if not os.path.exists(storage_path) :
         os.makedirs(storage_path)
 
-  #  if not os.path.exists(storage_path_) :
-  #      os.makedirs(storage_path_)
+    if not os.path.exists(storage_path_) :
+        os.makedirs(storage_path_)
 
   #  if not os.path.exists(storage_path__) :
   #      os.makedirs(storage_path__)
@@ -44,7 +44,7 @@ def main(name,storage,ninp,device='cpu'):
 
     ninp+=1
 
-#    model=NBeatsNet(ninp, device=device, forecast_length=forecast_length, backcast_length=backcast_length,block_type='fully_connected')
+    model=NBeatsNet(ninp, device=device, forecast_length=forecast_length, backcast_length=backcast_length,block_type='fully_connected')
     model__=NBeatsNet(ninp, device=device, forecast_length=forecast_length, backcast_length=backcast_length,block_type='Tr')
 #    model_=TransformerModel(ninp, nhead=2, nhid=128, nlayers=2, backast_size=backcast_length, forecast_size=forecast_length, dropout=0.5, device=device)
 
@@ -54,16 +54,16 @@ def main(name,storage,ninp,device='cpu'):
         print(f"Layer: {layer_name} | Size: {param.size()} \n")
 
     start_time=time.time()
- #   model.fit(xtrain, ytrain, xtest, ytest, name1, epochs=epochs, batch_size=bsz)
-    model__.fit(xtrain, ytrain, xtest, ytest, name_, epochs=epochs, batch_size=bsz)
+    model.fit(xtrain[:,:,:ninp], ytrain[:,:,:ninp], xtest[:,:,:ninp], ytest[:,:,:ninp], name1, epochs=epochs, batch_size=bsz)
+    model__.fit(xtrain[:,:,:ninp], ytrain[:,:,:ninp], xtest[:,:,:ninp], ytest[:,:,:ninp], name_, epochs=epochs, batch_size=bsz)
  #   model_.fit(xtrain, ytrain, xtest, ytest, bsz, epochs, name2)
     
     elapsed_time=time.time()-start_time
  #   test_loss = model.evaluate(xtest, ytest, eval_bsz, storage, False, save=False)
  #   train_loss = model.evaluate(xtrain, ytrain, bsz, storage, True, save=False)
-    print('=' * 89)
-    print('| End of training | test loss {:5.2f} | train loss {:5.2f} | '.format(test_loss, train_loss))
-    print('=' * 89)
+    # print('=' * 89)
+    # print('| End of training | test loss {:5.2f} | train loss {:5.2f} | '.format(test_loss, train_loss))
+    # print('=' * 89)
     print('| DL Session took {} seconds |'.format(elapsed_time))    
 
     print('---------- Name of the file : {} --------------'.format(storage))
