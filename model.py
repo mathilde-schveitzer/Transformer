7c0bf4874f1c282c7c8a1b4fb758e62d014a81ce
mathilde-schveitzer <mathilde.schvtzr@gmail.com>
2021-06-09 10:31:12 +0200

    learning positionnal encoding

--- model.py
+++ model.py
@@ -174,14 +174,11 @@ class PositionnalEncoding(nn.Module):
 
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
-#        pe[:, 0::2] = torch.sin(position)
         pe[:, 1::2] = torch.cos(position * div_term)
-#        pe[:, 1::2] = torch.cos(position)
         pe = pe.unsqueeze(0).transpose(0, 1)
-        self.register_buffer('pe', pe)
+       # self.register_buffer('pe', pe)
         self.device=device
 
     def forward(self, x):
