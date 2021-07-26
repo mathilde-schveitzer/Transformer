fe90c0e5fbf3fe71afa3027103ddfaae9411f6cc
mathilde-schveitzer <mathilde.schvtzr@gmail.com>
2021-07-26 16:31:45 +0200

    test

--- merge_model.py
+++ merge_model.py
@@ -23,7 +23,7 @@ class MergedModel(nn.Module):
 
         self.encoder = NBeatsNet(ninp, forecast_length=forecast_length, backcast_length=backcast_length, device=device)
         self.embed_dims=ninp*nhead
-        decoder_layers = TransformerDecoderLayer(self.embed_dims, nhead, dim_feedforward=128, dropout=0.2, activation='gelu')
+        decoder_layers = TransformerDecoderLayer(self.embed_dims, nhead, dim_feedforward=256, dropout=0.2, activation='gelu')
         self.converter = nn.Linear(ninp, self.embed_dims)
         self.decoder = TransformerDecoder(decoder_layers, num_layers=2)
         self.deconverter = nn.Linear(self.embed_dims, ninp)
@@ -31,7 +31,7 @@ class MergedModel(nn.Module):
 
         self.parameters=[]
         self.parameters=nn.ParameterList(self.parameters)
-        self._opt = torch.optim.Adam(self.parameters(), lr=1e-5, amsgrad=True)
+        self._opt = torch.optim.Adam(self.parameters(), lr=1e-4, amsgrad=True)
         self._loss = F.l1_loss
         self.to(self.device)
 
